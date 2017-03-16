d426493487c89594c44e25ee17133500efeeecb7
Ramesh Kurichi Ponnuswamy <rameshkp@ie.ibm.com>
2017-03-16 17:07:38 +0000

    Add loading button when posts load

--- src/app/posts.component.ts
+++ src/app/posts.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit} from "@angular/core";
 import {PostService} from "./post.service";
 import {Post} from "./post";
+import 'rxjs/add/operator/delay';
 
 @Component({
   templateUrl: '/app/posts.component.html'
 })
 export class PostsComponent implements OnInit {
   posts : Post[];
+  isLoading = true;
 
   constructor(
     private _ps: PostService
@@ -15,7 +17,10 @@ export class PostsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this._ps.getPosts().subscribe(posts => this.posts = posts);
+    this._ps.getPosts().delay(2000).subscribe(posts => {
+      this.posts = posts;
+      this.isLoading = false;
+    });
   }
 
 
